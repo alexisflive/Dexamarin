66d3ad4d3bfad7d5ace69549b1ed7912ba214c14
Alexis Ferreira <alexferreira@live.com>
2022-01-30 21:54:36 +0000

    Small improvements

--- dexamarin.py
+++ dexamarin.py
@@ -23,26 +23,23 @@ SOFTWARE.
 '''
 
 '''
-# Requirements - Install the following:
-* dotnet
-* ilspycmd: https://github.com/icsharpcode/ILSpy/tree/master/ICSharpCode.Decompiler.Console
-* "lz4" (pip) or "python3-lz4" (Debian, Ubuntu)
+DEPENDENCIES:
+  * .NET Core 3.1: https://dotnet.microsoft.com/en-us/download/dotnet/3.1
+  * ilspycmd: https://github.com/icsharpcode/ILSpy/tree/master/ICSharpCode.Decompiler.Console
+  * "lz4" (pip) or "python3-lz4" (Debian, Ubuntu)
 '''
 
 
-import glob
 import os
 import shutil
 import struct
 import sys
 import lz4.block
-
-def main(apkPath, outDirName):
-    os.mkdir(outDirName)
-    decompileAssemblies(apkPath, outDirName)
         
 
 def decompileAssemblies(apkPath, outDirName):
+    header_xalz_magic = b'XALZ'
+
     os.system('unzip {} -d {}'.format(apkPath, outDirName))
 
     for fname in os.listdir(outDirName):
@@ -55,26 +52,25 @@ def decompileAssemblies(apkPath, outDirName):
     for root, dirs, files in os.walk(outDirName + "/assemblies"):
         for file in files:
             if file.endswith('.dll'):
-                decompressXalz(root + "/" + file)
+                with open(root + "/" + file, "rb") as ofile:
+                    data = ofile.read()
+                    if data[:4] == header_xalz_magic:
+                        decompressXalz(ofile.name, data)
 
-    for root, dirs, files in os.walk(outDirName + "/assemblies"):
-        for file in files:
-            if file.endswith('.dll'):
-                decompDll = root + "/" + file
-                os.system('ilspycmd {} > {}.cs'.format(decompDll, decompDll))
-                print('{} decompiled'.format(decompDll))
+                    os.system('ilspycmd {} > {}'.format(ofile.name, ofile.name.replace(".dll", ".cs")))
+                    os.remove(ofile.name)
+                    print('{} was successfully decompiled'.format(ofile.name))
+
+    for fname in os.listdir(outDirName + "/assemblies/"):
+        shutil.move(outDirName + "/assemblies/" + fname, outDirName)
+
+    shutil.rmtree(outDirName + "/assemblies")
+    print("{} successfully decompiled!".format(apkPath))
+
 
 # modified from:
 # https://raw.githubusercontent.com/x41sec/tools/master/Mobile/Xamarin/Xamarin_XALZ_decompress.py
-def decompressXalz(inputFilepath):
-    header_xalz_magic = b'XALZ'
-    
-    with open(inputFilepath, "rb") as xalz_file:
-        data = xalz_file.read()
-    
-        if data[:4] != header_xalz_magic:
-            print("No XALZ decompression needed for {}".format(inputFilepath))
-        else:
+def decompressXalz(inFilePath, data):
             header_index = data[4:8]
             header_uncompressed_length = struct.unpack('<I', data[8:12])[0]
             payload = data[12:]
@@ -85,17 +81,15 @@ def decompressXalz(inputFilepath):
             
             decompressed = lz4.block.decompress(payload, uncompressed_size=header_uncompressed_length)
                     
-            with open(inputFilepath + "_decomp.dll", "wb") as output_file:
+            with open(inFilePath, "wb") as output_file:
                 output_file.write(decompressed)
                 output_file.close()
 
-            print("result written to file")
-            os.remove(inputFilepath)
+            print("{} was xalz decompressed".format(inFilePath))
 
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    if len(args) != 2:
-        print('DeXamarin: exactly two arguments required')
-    else:
-        main(args[0], args[1])
+    apkPath = sys.argv[1:][0]
+    outDirName = apkPath.replace(".apk", ".assemblies")
+    os.mkdir(outDirName)
+    decompileAssemblies(apkPath, outDirName)
